4cf29401053d2901e2ed5bffbc715450ce846312
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-02 23:57:34 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,4 +52,11 @@ class Owner
     self.pets[:cats].each do |cat|
       cat.mood = "happy"
     end
+  end
+
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
 end
